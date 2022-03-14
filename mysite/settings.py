230fdaaeb0f5b4d3e9e727f39f982154f3b245ff
Juanpix77 <98853853+Juanpix77@users.noreply.github.com>
2022-03-14 18:59:35 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-te$$14w#82kk6c83nlg%8a07naj74t#2m0myk5o-!kw_xf!b0+'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
